0d0410ed6149d0a3440e7d2b632a2e729383e416
Judy Milner <judy.milner@gmail.com>
2021-03-04 17:16:24 +0000

    Co-authored-by: peteredbrooke <peteredbrooke@users.noreply.github.com>

--- src/components/ForgotPassword/ForgotPassword.jsx
+++ src/components/ForgotPassword/ForgotPassword.jsx
@@ -1,11 +1,31 @@
-import React from "react"
-import styles from './ForgotPassword.module.scss';
-
+import React, { useState } from "react";
+import styles from "./ForgotPassword.module.scss";
 
 const ForgotPassword = () => {
+  const [notify, setBeenNotified] = useState(false);
+
+  const handleClick = () => {
+    if (notify == true) {
+      setBeenNotified(false);
+      // setBeenNotified to false
+    } else {
+      setBeenNotified(true);
+      // ... or the other way around
+    }
+  };
+
   return (
-        "New password enter now !"
+    <div>
+      <label for="email">Enter your email:</label>
+
+      {
+        // ..  if statement should show the notification or hide based on whether "notify" is true/false
+      }
+      <button onClick={handleClick}>Submit</button>
+
+      <section class={styles.alert}>Notificaton</section>
+    </div>
   );
-}
+};
 
 export default ForgotPassword;
