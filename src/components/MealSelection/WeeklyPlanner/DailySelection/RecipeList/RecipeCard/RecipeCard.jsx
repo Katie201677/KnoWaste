c34f01f7f2958da2275a4367b2437469d906015f
Tea <teamilas@hotmail.com>
2021-03-06 20:46:26 +0000

    clicking on img opens next day

--- src/components/MealSelection/WeeklyPlanner/DailySelection/RecipeList/RecipeCard/RecipeCard.jsx
+++ src/components/MealSelection/WeeklyPlanner/DailySelection/RecipeList/RecipeCard/RecipeCard.jsx
@@ -3,11 +3,14 @@ import styles from './RecipeCard.module.scss';
 
 const RecipeCard = (props) => {
   //Each individual recipe information in a card format.
+  const {action} = props;
   const {type, recipeName, dietaryrestriction} = props.mealChoice;
+  
+
   return (
     <div className={styles.recipeCard}>
       <section>
-        <img src="https://cafedelites.com/wp-content/uploads/2018/04/Best-Chicken-Tikka-Masala-IMAGE-2.jpg" alt={type} className={styles.recipeImg}/>
+        <img src="https://cafedelites.com/wp-content/uploads/2018/04/Best-Chicken-Tikka-Masala-IMAGE-2.jpg" alt={type} className={styles.recipeImg} onClick={action}/>
         <h3>{recipeName}</h3>
       </section>
       <section className={styles.dietRestrictions}>
--- src/components/MealSelection/WeeklyPlanner/DailySelection/RecipeList/RecipeList.jsx
+++ src/components/MealSelection/WeeklyPlanner/DailySelection/RecipeList/RecipeList.jsx
@@ -4,7 +4,9 @@ import styles from './RecipeList.module.scss';
 
 
 
-const RecipeList = () => {
+const RecipeList = (props) => {
+  const {action} = props;
+
   //Holds and organises the daily selection of food.
   const mealsOptions = [{type: "meat", recipeName: "Chicken Tikka Masala", dietaryrestriction: ["nuts", "gluten", "lactose"]}, {type: "fish", recipeName: "I am a fish", dietaryrestriction: ["nuts", "gluten", "lactose"]}, {type: "vegetarian", recipeName: "A single brocolli", dietaryrestriction: ["nuts", "gluten", "lactose"]}]
   const meal = mealsOptions.map(meal => {
@@ -15,9 +17,9 @@ const RecipeList = () => {
   return (
     <div className={styles.recipeList}>
       <section className={styles.transparentMeals}> 
-        <RecipeCard mealChoice = {meal[0]}/>
-        <RecipeCard mealChoice = {meal[1]}/>
-        <RecipeCard mealChoice = {meal[2]}/>
+        <RecipeCard mealChoice = {meal[0]} action={action}/>
+        <RecipeCard mealChoice = {meal[1]} action={action}/>
+        <RecipeCard mealChoice = {meal[2]} action={action}/>
       </section>
       
     </div>
