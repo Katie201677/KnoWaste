9f1bfc004baab09a8bcfaed363e1015d42b66c82
tim <tim>
2021-03-08 17:27:56 +0000

    Basic page layout started

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,26 +1,39 @@
-import react from 'react';
-import styles from './Login.module.scss';
-import asdwe from '../../assets/logoNoText.svg'
-
+import React from "react";
+import styles from "./Login.module.scss";
+import logo from "../../assets/logoNoText.svg";
 
 const Login = () => {
   return (
-    <div > {/* blue and purple container */}
-      {/* LOGO */}
-      <img src={asdwe} alt="logo"/>
-      {/* vertical line to separate logo from fields (desktop/tablet) */}
-      {/* email label */}
-      {/* email input */}
-      {/* password label */}
-      {/* password input */}
-      {/* show password button */}
-      {/* forgot password link */}
-      {/* login button hyperlink */}
-      {/* sign up hyperlink */}
-
+    <div className={styles.container}>
+      <div className={styles.loginPage}>
+        {" "}
+        {/* blue and purple container */}
+        {/* LOGO */}
+        <img src={logo} alt="logo" className={styles.logo} />
+        {/* vertical line to separate logo from fields (desktop/tablet) */}
+        <form action="" className={styles.loginForm}>
+          {/* email label */}
+          <label for="emailInput">Email </label>
+          {/* email input */}
+          <input type="email" id="emailInput" />
+          {/* password label */}
+          <label for="passwordInput">Password </label>
+          {/* password input */}
+          <input type="password" id="passwordInput" />
+          {/* show password button */}
+          {/* forgot password link */}
+          <a href="http://www.google.com">Forgot your password?</a>
+          {/* login button */}
+          <input type="submit" value="LOG IN" id={styles.loginButton} />
+          {/* sign up hyperlink */}
+          <span>
+            Don't have an account? <a href="/Register"> Sign up</a> here
+          </span>
+        </form>
+      </div>
     </div>
   );
-}
+};
 
 //figure out image in background
 
