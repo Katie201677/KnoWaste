1227d6fbabced85f9692c59379f6c1541b491667
Kirsty Clark <kclarkk14@gmail.com>
2021-03-08 14:38:55 +0000

    homepage skeleton

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,8 +3,24 @@ import styles from './Home.module.scss';
 
 const Home = () => {
   return (
-    "hello Home :-)"
+    <div>
+      {/*timer*/}
+      {/*info re timer (?)*/}
+      {/*improvements since last week etc*/}
+      <div className='linkContainer'>
+        <a href='./MealSelection'>
+          <img className='mealSelection'></img>
+        </a>
+        <a href='./AboutUs'>
+          <img className='aboutUs'></img>
+        </a>
+        <a href='./Gamification'>
+          <img className='gamification'></img>
+        </a>
+      </div>
+      {/*nav bar*/}
+    </div>
   );
-}
+};
 
-export default Home;
+export default Home
