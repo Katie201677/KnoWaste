e0758976a1f243091fc5e3e484eb277fcee0ba99
Alex Jordan - Dev <alexjordan-dev@Alexs-MacBook-Pro-2.local>
2021-03-09 14:51:29 +0000

    styling and logo for login page

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,15 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Login.module.scss";
-import logo from "../../assets/logoNoText.svg";
 
 const Login = () => {
+
+  const togglePassword = () => {
+    setIsVisible(!isVisible)
+  }
+  
+  const [isVisible, setIsVisible] = useState(false)
+
   return (
     <div className={styles.container}>
       <div className={styles.loginPage}>
         {" "}
         {/* blue and purple container */}
         {/* LOGO */}
-        <img src={logo} alt="logo" className={styles.logo} />
+        <img src="./knoWasteLogo_white.png" alt="logo" className={styles.logo} />
         {/* vertical line to separate logo from fields (desktop/tablet) */}
         <form action="" className={styles.loginForm}>
           {/* email label */}
@@ -19,22 +25,30 @@ const Login = () => {
           {/* password label */}
           <label for="passwordInput">Password </label>
           {/* password input */}
-          <input type="password" id="passwordInput" />
+          <input type={isVisible ? 'text':'password'} id="passwordInput" />
           {/* show password button */}
+          <span className={styles.showPass} onClick={togglePassword}>show</span>
           {/* forgot password link */}
-          <a href="http://www.google.com">Forgot your password?</a>
+          <span className={styles.smallText}>
+          Forgot your password?<a href="http://www.google.com"> Reset</a>
+          </span>
           {/* login button */}
           <input type="submit" value="LOG IN" id={styles.loginButton} />
           {/* sign up hyperlink */}
-          <span>
-            Don't have an account? <a href="/Register"> Sign up</a> here
+          <span className={styles.smallText}>
+            Don't have an account? <a href="/Register"> Sign up</a>
           </span>
         </form>
       </div>
     </div>
   );
+
+
+
+
+
 };
 
-//figure out image in background
+
 
 export default Login;
