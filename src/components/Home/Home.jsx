94e9199c183355092b42d5a1d25d92a97f3f382f
Alexander Allum <alexanderallum@Alexanders-MacBook-Pro.local>
2021-03-09 15:52:27 +0000

    break points stlyes done

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,7 +2,9 @@ import React from "react"
 import NavBar from "../NavBar";
 import styles from './Home.module.scss';
 import { Link } from 'react-router-dom';
-import picture from '../../assets/images/photo-canteen.jpg';
+import logo from '../../assets/images/Logo.png';
+import fork from '../../assets/images/fork.png';
+import leagueTable from '../../assets/images/leagueTable.png';
 
 const Home = () => {
   return (
@@ -24,21 +26,22 @@ const Home = () => {
           <div className={styles.mealSelection}>
             <Link to='/mealselection' >
               <h3>Meal Selection</h3>
-              <img className='mealSelection'></img>
+              <img src={fork} className={styles.fork}></img>
+              <p className={styles.textMeal}>Tonight you're having chicken curry!</p>
             </Link>
           </div>
         </div>
         <div className={styles.linkContainerTwo}>
           <div className={styles.aboutUs}>
             <Link to='/aboutus'>
-            <h3>About Us / Environment</h3>
-            <img className='aboutUs'></img>
+            <h3>Your Impact</h3>
+            <img src={logo} className={styles.logo}></img>
             </Link>
           </div>
           <div className={styles.gamification}>
             <Link to='/gamification'>
-              <h3>Gamification</h3>
-              <img className='gamification'></img>
+              <h3>League Tables</h3>
+              <img src={leagueTable} className={styles.leagueTable}></img>
             </Link>
           </div>
         </div>
