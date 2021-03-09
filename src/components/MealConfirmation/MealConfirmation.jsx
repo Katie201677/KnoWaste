dd981675f3b56087c55bd557557a60ec747665e0
Judy Milner <judy.milner@gmail.com>
2021-03-09 16:36:05 +0000

    Confirmation page skeleton start

--- src/components/MealConfirmation/MealConfirmation.jsx
+++ src/components/MealConfirmation/MealConfirmation.jsx
@@ -1,10 +1,58 @@
-import React from "react"
+import React from 'react'
+import kitchenImage from '../../assets/cafe.jpg';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import library from '../../data/fa-library.js';
 import styles from './MealConfirmation.module.scss';
 
+
 const MealConfirmation = () => {
-  return (
-    "your meals for this week are x y and z"
-  );
+    return (
+        <main style={{backgroundImage: `url(${kitchenImage})`
+  }}>
+            <div className="mainDiv">
+                <h1>Your Meal Choices</h1>
+                <section className="confirmMenu">
+                    <ul>
+                        <li>
+                            <span className="dot"></span>
+                            <h3>Monday</h3>
+                            <p>Yummy Monday Dinner</p>
+                            {/* Allergen symbols */}
+                            <FontAwesomeIcon icon="pen"/>
+                        </li>
+                        <li>
+                            <h3>Tuesday</h3>
+                            <p>Yummy Tuesday Dinner</p>
+                            {/* Allergen symbols */}
+                            <FontAwesomeIcon icon="pen"/>
+                        </li>
+                        <li>
+                            <h3>Wednesday</h3>
+                            <p>Yummy Wednesday Dinner</p>
+                            {/* Allergen symbols */}
+                            <FontAwesomeIcon icon="pen"/>
+                        </li>
+                        <li>
+                            <h3>Thurdsday</h3>
+                            <p>Yummy Thursday Dinner</p>
+                            {/* Allergen symbols */}
+                            <FontAwesomeIcon icon="pen"/>
+                        </li>
+                        <li>
+                            <h3>Friday</h3>
+                            <p>you have to go to TGI Fridays on Fridays it is law.</p>
+                            {/* Allergen symbols */}
+                            {/* Edit Button */}
+                            <FontAwesomeIcon icon="pen"/>
+                        </li>
+                    </ul>
+                </section>
+                <button>CHECKOUT</button>
+            </div>
+        </main>
+    )
 }
 
 export default MealConfirmation;
+
+
---
+++ src/data/fa-library.js
@@ -0,0 +1,5 @@
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+import { faPen } from "@fortawesome/free-solid-svg-icons";
+
+export default library.add(faPen);
