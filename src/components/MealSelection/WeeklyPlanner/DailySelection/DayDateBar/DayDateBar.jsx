e17561d6ed2027e6b5eda9c708ac59bc72fe2be2
Tea <teamilas@hotmail.com>
2021-03-09 13:51:49 +0000

    cleaned up a bit of code

--- src/components/MealSelection/WeeklyPlanner/DailySelection/DayDateBar/DayDateBar.jsx
+++ src/components/MealSelection/WeeklyPlanner/DailySelection/DayDateBar/DayDateBar.jsx
@@ -3,24 +3,19 @@ import styles from './DayDateBar.module.scss';
 import PropTypes from 'prop-types';
 
 const DayDateBar = (props) => {
-    const {date, setActiveDate, activeDate, isSelected, activeMeal, action} = props;
+    const {date, activeDate, isSelected, activeMeal, action} = props;
 
-
-    const openDaySelection = () => {
-        setActiveDate(date);
-    }
 
     return (
         
         <div  className={ date == activeDate ? styles.diffColour : styles.dayBar}  >
             <h2>{date}</h2>
             {
-                date == activeDate ? <button className={styles.btn} onClick={action}>SKIP THE MEAL</button> : ""
+                date == activeDate ? <button className={styles.btn} onClick={action}> SKIP THE MEAL</button> : ""
             }
             {   
                isSelected == true ? <h5>{activeMeal}</h5> : ""
            }  
-           {console.log("activeMeal",activeMeal)}
         </div>
     )
 }
