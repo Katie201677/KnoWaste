efedba7a51cd4de51c7d1947fe653506a1034716
Seb Lee <sebastian.ma.lee@gmail.com>
2021-03-10 17:12:50 +0000

    more styling tweaks

--- src/components/MealConfirmation/MealCard/MealCard.jsx
+++ src/components/MealConfirmation/MealCard/MealCard.jsx
@@ -1,12 +1,23 @@
-import React from 'react'
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import styles from "./MealCard.module.scss";
 
 const MealCard = (prop) => {
+  const { day, meal } = prop;
   return (
     <li>
-      <h3>{prop.day}</h3>
-      <p>{prop.meal}</p>
+      <div>
+        <FontAwesomeIcon icon="arrow-right" className={styles.arrowIcon} />
+      </div>
+      <div className = {styles.mealInfo}>
+          <h3>{day}</h3>
+          <p>{meal}</p>
+      </div>
+      <div>
+        <FontAwesomeIcon icon="pen" className={styles.penIcon} />
+      </div>
     </li>
-  )
-}
+  );
+};
 
-export default MealCard
+export default MealCard;
--- src/components/MealConfirmation/MealConfirmation.jsx
+++ src/components/MealConfirmation/MealConfirmation.jsx
@@ -1,36 +1,31 @@
-import React from 'react'
-import kitchenImage from '../../assets/cafe.jpg';
+import React from "react";
+import kitchenImage from "../../assets/cafe.jpg";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import library from '../../data/fa-library.js';
-import styles from './MealConfirmation.module.scss';
-import MealCard from './MealCard'
-
+import library from "../../data/fa-library.js";
+import styles from "./MealConfirmation.module.scss";
+import MealCard from "./MealCard";
 
 const MealConfirmation = () => {
-  const days = ["Monday","Tuesday", "Wednesday", "Thursday", "Friday"];
-    return (
-        <main style={{backgroundImage: `url(${kitchenImage})`
-  }}>
-    <h1>Your Meal Choices</h1>
-            <div className={styles.mainDiv}>
-            
-                <section className={styles.confirmMenu}>
-                  {
-                    days.map(day =>(
-                    <>
-                      <MealCard day={day} meal="meal"/>
-                    </>
-                    ))
-                  }
-                   
-        
-                </section>
-                <button>CHECKOUT</button>
-            </div>
-        </main>
-    )
-}
+  const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
+  return (
+    <main style={{ backgroundImage: `url(${kitchenImage})` }}>
+      <div className={styles.mainDiv}>
+        <h1>Your Meal Choices</h1>
+        <section className={styles.confirmMenu}>
+          {
+            <ul>
+              {days.map((day) => (
+                <>
+                  <MealCard day={day} meal="Yummy meal" />
+                </>
+              ))}
+            </ul>
+          }
+        </section>
+        <button>CHECKOUT</button>
+      </div>
+    </main>
+  );
+};
 
 export default MealConfirmation;
-
-
