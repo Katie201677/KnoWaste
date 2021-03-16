e3ebe5da2c2fb0c1c3dfadd6064b4c40da930595
gel00 <gellaigabor@gmail.com>
2021-03-16 18:02:31 +0000

    validation

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -3,8 +3,6 @@ import styles from "./Register.module.scss";
 import { useForm } from "react-hook-form";
 
 const Register = () => {
-  const { register, handleSubmit, errors, watch } = useForm();
-  
   const onSubmit = (data) => {
     console.log(data);
   };
@@ -15,13 +13,12 @@ const Register = () => {
   // const [formComplete, setFormComplete] = useState(false)
 
   // Object.values(watch()).every(value => value) === formComplete ? '' : setFormComplete(!formComplete);
-  
-  let formComplete = Object.values(watch()).every(value => value)
 
-  console.log(formComplete)
+  //let formComplete = Object.values(watch()).every(value => value)
 
+  //console.log(formComplete)
 
-/*
+  /*
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
 
@@ -61,49 +58,116 @@ const Register = () => {
     </form>
   );
 */
+  const { register, handleSubmit, errors, watch } = useForm();
+  console.log(watch());
   return (
     <div className={styles.pageBody}>
       <div className={styles.mainFormContainer}>
-      <h3 className={styles.header}>Sign Up</h3>
-      <form onSubmit={handleSubmit} >
-        
-          <label htmlFor="name">Name</label>
-          <input id="name" name="name" type="text" placeholder="John Smith" ref={register}></input>
-
-          <label>Email</label>
-          <input name="email" type="email" placeholder="example@example.co.uk" ref={register}></input>
-
-          <label>Password</label>
-
+        <h3 className={styles.header}>Sign Up</h3>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <label htmlFor="name">Name<span className={styles.inputError}>{errors.name?.message}</span></label>
           <input
+            id="name"
+            name="name"
+            type="text"
+            placeholder="John Smith"
+            ref={register({
+              required: "Name is required.",
+            })}
+          ></input>
+
+          <label htmlFor="email">Email<span className={styles.inputError}>{errors.email?.message}</span></label>
+          
+          <input
+            id="email"
+            name="email"
+            type="text"
+            placeholder="example@example.co.uk"
+            ref={register({
+              required: "Email is required.",
+              pattern: {
+                value: /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+                message: "Invalid email address",
+              },
+            })}
+          ></input>
+
+          <label htmlFor="password">Password<span className={styles.inputError}>{errors.password?.message}</span></label>
+          
+          <input
+            id= "password"
             name="password"
             type="password"
-            minLength="8"
-            placeholder="Passwords must be at least 8 characters"
+            ref={register({
+              required: "Password is required.",
+              pattern: {
+                value: /[A-Za-z0-9]{8,}/,
+                message: "Minimum eight characters.",
+              },
+            })}
+          ></input>
+
+          <label>Student Number
+          <span className={styles.inputError}>{errors.studentNumber?.message}</span>
+          </label>
+          
+          <input
+            name="studentNumber"
+            type="number"
+            placeholder="123456"
+            ref={register({
+              minLength: {
+                value: 6,
+                message: "At least 6 numbers.",
+              },
+              required: "Student Number is required."
+            })}
+          ></input>
+
+          <label>Halls of Residence
+          <span className={styles.inputError}>{errors.residenceHall?.message}</span>
+          </label>
+          <select name="residenceHall" 
+            ref={register({
+              required: "Residence Hall is required."
+            })}
+          >
+            <option value="">----select hall----</option>
+            <option value="Badock Hall">Badock Hall</option>
+            <option value="Churchill Hall">Churchill Hall</option>
+            <option value="Clifton Hill House">Clifton Hill House</option>
+            <option value="Hiatt Baker Hall">Hiatt Baker Hall</option>
+            <option value="Wills Hall">Wills Hall</option>
+          </select>
+
+          <label>Date of Birth
+          <span className={styles.inputError}>{errors.dob?.message}</span>
+          </label>
+          <input
+            name="dob"
+            type="date"
+            placeholder="DD/MM/YYYY"
+            ref={register({
+              required: "Date of Birth is required."
+            })}
+          ></input>
+
+          <label>Mobile Number</label>
+          <input
+            name="mobile"
+            type="tel"
+            placeholder="07717330113"
             ref={register}
           ></input>
 
-          <label>Student Number</label>
-          <input name="studentNumber" type="number" placeholder="123456" ref={register}></input>
-
-          <label>Halls of Residence</label>
-          <select name="residenceHall" ref={register}>
-            <option>Badock Hall</option>
-            <option>Churchill Hall</option>
-            <option>Clifton Hill House</option>
-            <option>Hiatt Baker Hall</option>
-            <option>Wills Hall</option>
-          </select>
-
-          <label>Date of Birth</label>
-          <input name="dob" type="date" placeholder="DD/MM/YYYY" ref={register}></input>
-
-          <label>Mobile Number</label>
-          <input name="mobile" type="tel" placeholder="07717330113" ref={register}></input>
-
-        <div className={styles.termsContainer}>
-          <label>Terms and Conditions</label>
-          <textarea rows="5" cols="50" value="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae doloribus
+          <div className={styles.termsContainer}>
+            <label>Terms and Conditions
+            <span className={styles.inputError}>{errors.tAndCCheck?.message}</span>
+            </label>
+            <textarea
+              rows="5"
+              cols="50"
+              value="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae doloribus
             nihil qui, minima harum officia cum provident sit dicta adipisci optio
             sequi at soluta temporibus quo quia laborum illum dignissimos! Lorem
             ipsum dolor, sit amet consectetur adipisicing elit. Quo voluptatem labore
@@ -112,32 +176,49 @@ const Register = () => {
             earum quas voluptatibus? Lorem, ipsum dolor sit amet consectetur
             adipisicing elit. Inventore voluptatum distinctio, fugiat quis ex
             deserunt quasi molestias placeat ipsa quo aut ipsam itaque? Sequi
-            explicabo quidem consequatur cumque temporibus rerum?" readOnly />
-          {/* </textarea> */}
-        </div>
+            explicabo quidem consequatur cumque temporibus rerum?"
+              readOnly
+            />
+            {/* </textarea> */}
+          </div>
 
-        <div className={styles.tAndC}>
-          <input name="tAndCCheck" type="checkbox" ref={register({ checked: true })}/>
-          <p>
-            By ticking this box you aknowledge that you have read and agree to our
-            terms and conditions
-          </p>
-          <input name="advertisementCheck" type="checkbox" ref={register({ checked: true })}></input>
-          <p>
-            By ticking this box you agree to recieving occasional tailored
-            advertisments about our amazing products
-          </p>
-        </div>
+          <div className={styles.tAndC}>
+            <input
+              name="tAndCCheck"
+              type="checkbox"
+              ref={
+                register({ required: "You must agree to the Terms and Conditions." }
+              )}
+              value = {true}
+            />
+            <p>
+              By ticking this box you aknowledge that you have read and agree to
+              our terms and conditions
+            </p>
+            <input
+              value = {true}
+              ref={register}
+              name="advertisementCheck"
+              type="checkbox"
+            ></input>
+            <p>
+              By ticking this box you agree to recieving occasional tailored
+              advertisments about our amazing products
+            </p>
+          </div>
 
-        <div className={styles.signUpButtonContainer}>
-          <button type="submit" className={`${styles['btn-primary']} ${styles.button}`}>
-            Sign Up
-          </button>
-        </div>
-      </form>
+          <div className={styles.signUpButtonContainer}>
+            <button 
+              type="submit"
+              className={`${styles["btn-primary"]} ${styles.button}`}
+            >
+              Sign Up
+            </button>
+          </div>
+        </form>
       </div>
     </div>
-   );
+  );
 };
 
 export default Register;
