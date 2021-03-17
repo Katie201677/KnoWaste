2e0e417a9bc196b3aa90bc2c79a8ef3053e249cb
Jchenhall <chenhall_j@hotmail.co.uk>
2021-03-17 14:54:23 +0000

    AdminMealInput page validation done.

--- src/components/Admin/AdminMealInput/AdminMealInput.jsx
+++ src/components/Admin/AdminMealInput/AdminMealInput.jsx
@@ -4,10 +4,14 @@ import mealLibrary from "../AdminLibrary/AdminLibrary.json";
 import { useForm } from "react-hook-form";
 
 const AdminMealInput = () => {
-  const { register, handleSubmit } = useForm();
+  const { register, handleSubmit, errors} = useForm();
   const onSubmit = (data) => {
     console.log(data);
     mealLibrary.push(data);
+   
+    let newData = data;
+    newData.mealIncludes = data.mealIncludes.split(", ")
+    console.log(newData);
     //push a random ID to it
     //split mealIncludes and return as array
     //push it to adminLibrary
@@ -23,23 +27,27 @@ const AdminMealInput = () => {
           type="text"
           placeholder="Name"
           name="mealName"
-          ref={register}
+          ref={register ({required : true})}
         ></input>
+                  {errors.mealName && errors.mealName.type === "required" && (<p>This field is required</p>)}
+
         <label>Description of Meal</label>
         <textarea
           type="text"
           placeholder="Description"
           name="mealDescription"
-          ref={register}
+          ref={register ({required : true})}
         ></textarea>
+        {errors.mealDescription && errors.mealDescription.type === "required" && (<p>This field is required</p>)}
         <button>Upload Image</button>
         <div className={styles.imgPreview}></div>
         <h3>Meal Type</h3>
-        <select name="mealType" ref={register}>
+        <select name="mealType" ref={register ({required : true})}>
           <option value="main">Main</option>
           <option value="side">Side</option>
           <option value="dessert">Dessert</option>
         </select>
+        {errors.mealType && errors.mealType.type === "required" && (<p>This field is required</p>)}
         <h3>Allergens</h3>
         <label>Gluten</label>
         <input
@@ -47,24 +55,27 @@ const AdminMealInput = () => {
           name="mealAllergens"
           value="gluten"
           className={styles.checkBox}
-          ref={register}
+          ref={register }
         />
+        
         <label>Dairy</label>
         <input
           type="checkbox"
           name="mealAllergens"
           value="dairy"
           className={styles.checkBox}
-          ref={register}
+          ref={register }
         />
+         
         <label>Shellfish</label>
         <input
           type="checkbox"
           name="mealAllergens"
           value="shellfish"
           className={styles.checkBox}
-          ref={register}
+          ref={register }
         />
+          
 
         <h3>Dietary Requirements</h3>
         <label>Vegetarian</label>
@@ -72,25 +83,30 @@ const AdminMealInput = () => {
           type="checkbox"
           name="mealDiet"
           value="vegetarian"
-          ref={register}
+          ref={register }
         />
         <label>Vegan</label>
-        <input type="checkbox" name="mealDiet" value="vegan" ref={register} />
+        <input type="checkbox" name="mealDiet" value="vegan" ref={register } />
+
         <label>Halal</label>
         <input type="checkbox" name="mealDiet" value="halal" ref={register} />
+        
 
         <h3>Meal Includes</h3>
         <input
           type="text"
           placeholder="potato, leek, cheese"
           name="mealIncludes"
-          ref={register}
+          ref={register ({required : true})}
         ></input>
+        {errors.mealIncludes && errors.mealIncludes.type === "required" && (<p>This field is required</p>)}    
 
         <label>Carbon Footprint</label>
-        <input type="number" name="mealCarbon" ref={register}></input>
+        <input type="number" name="mealCarbon" ref={register ({required : true})}></input>
+        {errors.mealCarbon && errors.mealCarbon.type === "required" && (<p>This field is required</p>)} 
         <label>Water Usage</label>
-        <input type="number" name="mealWater" ref={register}></input>
+        <input type="number" name="mealWater" ref={register ({required : true})}></input>
+        {errors.mealWater && errors.mealWater.type === "required" && (<p>This field is required</p>)} 
 
         <button>Preview</button>
         <input type="submit" />
