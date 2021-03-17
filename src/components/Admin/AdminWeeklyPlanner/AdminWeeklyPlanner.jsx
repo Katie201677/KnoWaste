b72226ae8f4af8928090c061d0e483c772a7f6d0
Jchenhall <chenhall_j@hotmail.co.uk>
2021-03-17 15:13:20 +0000

    AdminWeeklyPlanner form hook implemented

--- src/components/Admin/AdminWeeklyPlanner/AdminWeeklyPlanner.jsx
+++ src/components/Admin/AdminWeeklyPlanner/AdminWeeklyPlanner.jsx
@@ -1,162 +1,168 @@
 import React from "react";
 import styles from "./AdminWeeklyPlanner.module.scss";
-
+import { useForm } from "react-hook-form";
 
 
 const AdminWeeklyPlanner = () => {
+       const { register, handleSubmit} = useForm();
+  const onSubmit = (data) => {
+    console.log(data);
+  }
     return (
         <section>
+               <form onSubmit={handleSubmit(onSubmit)}>
              {/* As a developer I can select the day of the week */}
              <div className={styles.Monday}>
              <h3>Monday</h3>
              <h3>Main</h3>
-             <select>
-                    <option>meal1</option>
-                    <option>meal2</option>
-                    <option>meal3</option>
+             <select name="mondayMain" ref={register}>
+                    <option value="meal1">meal1</option>
+                    <option value="meal2">meal2</option>
+                    <option value="meal3">meal3</option>
              </select>
              <h3>Side</h3>
-             <select>
-                    <option>side1</option>
-                    <option>side2</option>
-                    <option>side3</option>
+             <select name="mondaySide" ref={register}>
+                    <option value="side1">side1</option>
+                    <option value="side2">side2</option>
+                    <option value="side3">side3</option>
              </select>
              <h3>Dessert</h3>
-             <select>
-                    <option>des1</option>
-                     <option>des2</option>
-                     <option>des3</option>
+             <select name="mondayDes" ref={register}>
+                    <option value="des1">des1</option>
+                     <option value="des2">des2</option>
+                     <option value="des3">des3</option>
              </select>
              </div>
              <div className={styles.Tuesday}>
              <h3>Tuesday</h3>
              <h3>Main</h3>
-             <select>
-                    <option>meal1</option>
-                    <option>meal2</option>
-                    <option>meal3</option>
+             <select name="tuesdayMain" ref={register}>
+                    <option value="meal1">meal1</option>
+                    <option value="meal2">meal2</option>
+                    <option value="meal3">meal3</option>
              </select>
              <h3>Side</h3>
-             <select>
-                    <option>side1</option>
-                    <option>side2</option>
-                    <option>side3</option>
+             <select name="tuesdaySide" ref={register}>
+                    <option value="side1">side1</option>
+                    <option value="side2">side2</option>
+                    <option value="side3">side3</option>
              </select>
              <h3>Dessert</h3>
-             <select>
-                    <option>des1</option>
-                     <option>des2</option>
-                     <option>des3</option>
+             <select name="tuesdayDes" ref={register}>
+                    <option value="des1">des1</option>
+                     <option value="des2">des2</option>
+                     <option value="des3">des3</option>
              </select>
             </div>
             <div className={styles.Wednesday}>
             <h3>Wednesday</h3>
              <h3>Main</h3>
-             <select>
-                    <option>meal1</option>
-                    <option>meal2</option>
-                    <option>meal3</option>
+             <select name="wednesdayMain" ref={register}>
+                    <option value="meal1">meal1</option>
+                    <option value="meal2">meal2</option>
+                    <option value="meal3">meal3</option>
              </select>
              <h3>Side</h3>
-             <select>
-                    <option>side1</option>
-                    <option>side2</option>
-                    <option>side3</option>
+             <select name="wednesdaySide" ref={register}>
+                    <option value="side1">side1</option>
+                    <option value="side2">side2</option>
+                    <option value="side3">side3</option>
              </select>
              <h3>Dessert</h3>
-             <select>
-                    <option>des1</option>
-                     <option>des2</option>
-                     <option>des3</option>
+             <select name="wednesdayDes" ref={register}>
+                    <option value="des1">des1</option>
+                     <option value="des2">des2</option>
+                     <option value="des3">des3</option>
              </select>
             </div>
             <div className={styles.Thursday}>
             <h3>Thursday</h3>
              <h3>Main</h3>
-             <select>
-                    <option>meal1</option>
-                    <option>meal2</option>
-                    <option>meal3</option>
+             <select name="thursdayMain" ref={register}>
+                    <option value="meal1">meal1</option>
+                    <option value="meal2">meal2</option>
+                    <option value="meal3">meal3</option>
              </select>
              <h3>Side</h3>
-             <select>
-                    <option>side1</option>
-                    <option>side2</option>
-                    <option>side3</option>
+             <select name="thursdaySide" ref={register}>
+                    <option value="side1">side1</option>
+                    <option value="side2">side2</option>
+                    <option value="side3">side3</option>
              </select>
              <h3>Dessert</h3>
-             <select>
-                    <option>des1</option>
-                     <option>des2</option>
-                     <option>des3</option>
+             <select name="thursdayDes" ref={register}>
+                    <option value="des1">des1</option>
+                     <option value="des2">des2</option>
+                     <option value="des3">des3</option>
              </select>
             </div>
             <div className={styles.Friday}>
             <h3>Friday</h3>
              <h3>Main</h3>
-             <select>
-                    <option>meal1</option>
-                    <option>meal2</option>
-                    <option>meal3</option>
+             <select name="fridayMain" ref={register}>
+                    <option value="meal1">meal1</option>
+                    <option value="meal2">meal2</option>
+                    <option value="meal3">meal3</option>
              </select>
              <h3>Side</h3>
-             <select>
-                    <option>side1</option>
-                    <option>side2</option>
-                    <option>side3</option>
+             <select name="fridaySide" ref={register}>
+                    <option value="side1">side1</option>
+                    <option value="side2">side2</option>
+                    <option value="side3">side3</option>
              </select>
              <h3>Dessert</h3>
-             <select>
-                    <option>des1</option>
-                     <option>des2</option>
-                     <option>des3</option>
+             <select name="fridayDes" ref={register}>
+                    <option value="des1">des1</option>
+                     <option value="des2">des2</option>
+                     <option value="des3">des3</option>
              </select>
             </div>
             <div className={styles.Saturday}>
             <h3>Saturday</h3>
              <h3>Main</h3>
-             <select>
-                    <option>meal1</option>
-                    <option>meal2</option>
-                    <option>meal3</option>
+             <select name="saturdayMain" ref={register}>
+                    <option value="meal1">meal1</option>
+                    <option value="meal2">meal2</option>
+                    <option value="meal3">meal3</option>
              </select>
              <h3>Side</h3>
-             <select>
-                    <option>side1</option>
-                    <option>side2</option>
-                    <option>side3</option>
+             <select name="saturdaySide" ref={register}>
+                    <option value="side1">side1</option>
+                    <option value="side2">side2</option>
+                    <option value="side3">side3</option>
              </select>
              <h3>Dessert</h3>
-             <select>
-                    <option>des1</option>
-                     <option>des2</option>
-                     <option>des3</option>
+             <select name="saturdayDes" ref={register}>
+                    <option value="des1">des1</option>
+                     <option value="des2">des2</option>
+                     <option value="des3">des3</option>
              </select>
             </div>
             <div className={styles.Sunday}>
             <h3>Sunday</h3>
              <h3>Main</h3>
-             <select>
-                    <option>meal1</option>
-                    <option>meal2</option>
-                    <option>meal3</option>
+             <select name="sundayMain" ref={register}>
+                    <option value="meal1">meal1</option>
+                    <option value="meal2">meal2</option>
+                    <option value="meal3">meal3</option>
              </select>
              <h3>Side</h3>
-             <select>
-                    <option>side1</option>
-                    <option>side2</option>
-                    <option>side3</option>
+             <select name="sundaySide" ref={register}>
+                    <option value="side1">side1</option>
+                    <option value="side2">side2</option>
+                    <option value="side3">side3</option>
              </select>
              <h3>Dessert</h3>
-             <select>
-                    <option>des1</option>
-                     <option>des2</option>
-                     <option>des3</option>
+             <select name="sundayDes" ref={register}>
+                    <option value="des1">des1</option>
+                     <option value="des2">des2</option>
+                     <option value="des3">des3</option>
              </select>
             </div>
-   
+            <input type="submit" />
+            </form>
         </section>
     )
-}
+};
 
 export default AdminWeeklyPlanner;
