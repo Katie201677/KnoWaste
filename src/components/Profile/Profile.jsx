a08bbc46e2cfb178dcfc5176d1384077f7cafa9a
Oliver Coldwell <olivercoldwell@gmail.com>
2021-03-18 16:41:31 +0000

    Updating style on profile page, work in progress

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,57 +1,61 @@
-import React from 'react';
-import styles from './Profile.module.scss';
-import EditProfile from "./EditProfile";
+import React from "react";
+import styles from "./Profile.module.scss";
 import { useState } from "react";
-
+import ProfilePic from "../../assets/kitchen_1.jpg";
+import NavBar from "../NavBar/";
+import Timer from "../MealSelection/Timer";
 
 // edit button -> use icons
 // This shows once edit details button selected
 const EditPicProfile = () => {
   return (
-    <div id={styles.editPicButton} className={styles.editButton}>+</div>
-  )
-}
+    <div id={styles.editPicButton} className={styles.editButton}>
+      +
+    </div>
+  );
+};
 const Profile = () => {
   const [isEditing, setIsEditing] = useState(false);
   const showEditProfile = () => {
     setIsEditing(!isEditing);
-  }
+  };
   return (
-    <div className={styles.profile}>
-    {/* profile picture tile */}
+    <div className="content">
+      <NavBar />
+      <Timer />
+      <div className={styles.mainSection} >
+        {/* profile picture tile */}
 
-    <div id={styles.profilePic} className={styles.profileContainer}>
-      <img className={styles.profilePic}></img>
-      <div id={styles.editPicButton} className={isEditing ? styles.editButton : styles.hidden}>+</div>
-      {/* full name, username/course title and halls will disappear when edit button clicked*/}
-      <div id={styles.profileInfo} className={isEditing ? styles.hidden : ''}>
-        <div id={styles.fullName} className={styles.profileText}>Woody woody</div>
-        <div id={styles.userName} className={styles.profileText}>woody99</div>
-        <div id={styles.halls} className={styles.profileText}>Woody Hall</div>
-      </div>
-      {/* edit profile button */}
-      <div id={styles.editProfileButton} className={styles.editButton} onClick={showEditProfile}>{isEditing ? "Save" : "Edit"}</div>
-    </div>
-
-    {/* dietary requirment summary */}
-    <div className={isEditing ? styles.hidden : styles.DietSection}>
-      <span className={styles.dietTile}>Diet 1</span>
-      <span className={styles.dietTile}>Diet 2</span>
-      <span className={styles.dietTile}>Diet 3</span>
-    </div>
-    <div className={isEditing ? '' : styles.hidden}>
-      <EditProfile />
-    </div>
-    {/* weekly score... data/metrics are input here */}
-      <div id={styles.score} className={isEditing ? styles.hidden : styles.stats}>
-        <h2>Weekly Score</h2>
-        <div className={styles.figures}>
-        <p>You helped save 30L of water</p>
-        <p>You helped save ... of CO2</p>
+        <div className={styles.profile}>
+          <section className={styles.profile__card}>
+            <section className={styles.profile__info}>
+              <img src={ProfilePic} className={styles.profile__pic}></img>
+              <p>
+                Woody<br></br>woody99<br></br> Woody Hall
+              </p>
+            </section>
+            <div className={styles.diet__info}>
+              <span className={styles.dietTile}>Diet 1</span>
+              <span className={styles.dietTile}>Diet 2</span>
+              <span className={styles.dietTile}>Diet 3</span>
+            </div>
+          </section>
+        <div className={isEditing ? "" : styles.hidden}></div>
+        {/* weekly score... data/metrics are input here */}
+        <div
+          id={styles.score}
+          className={isEditing ? styles.hidden : styles.stats}
+        >
+          <h2>Weekly Score</h2>
+          <div className={styles.figures}>
+            <p>You helped save 30L of water</p>
+            <p>You helped save ... of CO2</p>
+          </div>
         </div>
       </div>
-      {/*<NavBar />*/}
     </div>
+        </div>
+        
   );
-}
+};
 export default Profile;
