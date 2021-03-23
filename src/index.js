de2c9a2bcfb29e353d20b498f414c664b22c56c5
Oliver Coldwell <olivercoldwell@gmail.com>
2021-03-23 15:45:01 +0000

    "added firebase.js"

--- src/index.js
+++ src/index.js
@@ -2,24 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App.jsx';
+import "./firebase.js"
 import reportWebVitals from './reportWebVitals';
-import firebase from "firebase/app";
-import "firebase/auth";
 import {
   FirebaseAuthProvider
 } from "@react-firebase/auth";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyCJypedQSVDr-lIILrJWSltWjcUQaHv6oc",
-  authDomain: "knowaste-3c92c.firebaseapp.com",
-  projectId: "knowaste-3c92c",
-  storageBucket: "knowaste-3c92c.appspot.com",
-  messagingSenderId: "1031894047710",
-  appId: "1:1031894047710:web:1fa6e25d8b273608e89b4f",
-  measurementId: "G-52P45TXG7Y"
-};
-
-firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
