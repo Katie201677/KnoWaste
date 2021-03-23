af181b10f3b0a464583211d73881d9d2f7b8d838
gel00 <gellaigabor@gmail.com>
2021-03-23 17:44:57 +0000

    simplified rules

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -10,58 +10,54 @@ import HelpPopUp from '../HelpPopUp';
 
 const Home = () => {
 
-  return (
-    <div className={styles.content}>
-      <div className={styles.navBar}>
-        <NavBar />
-      </div>
-      <div className={styles.fullPage}>
-        <div className={styles.makeInline}>
-         <HelpPopUp sourcePage='homepage'/>
-        </div>
-    <div>
-      <div className={styles.linkContainer} >
-          
-          <div className={`${styles.mealSelection} box-style-1`}>
+return (
+<div className={styles.content}>
+  <div className={styles.navBar}>
+    <NavBar />
+  </div>
+  <div className={styles.fullPage}>
+  <div className={styles.makeInline}>
+      <HelpPopUp sourcePage='homepage' />
+    </div>
+      <div className={styles.linkContainer}>
+        <div className={`${styles.mealSelection} box-style-1`}>
           <h1 className="textwhite">Meal Selection</h1>
           <Link to="/mealselection">
-                <button className='button-style-1'>Click here to choose!</button>
-          </Link>               
+          <button className='button-style-1'>Click here to choose!</button>
+          </Link>
           <img src={Fork} className={styles.fork}></img>
-              
-          </div>
-          <div className={styles.timer}>
-            <Timer />
-          </div>
-          <div className={`${styles.aboutUs} box-style-1`}>
-            <div className={styles.imageContainer}>
-              <img src={Logo} className={styles.leagueTable}></img>
-          </div>
-            <h3 className="textwhite">Your Impact</h3>
-            <p className="textwhite">Well done! 10% waste less than last week.</p>
-            <Link to="environment">
-              <button className='button-style-1'>Explore</button>
-            </Link>
-          </div>
-        
-      
-            <div className={`${styles.gamification} box-style-1`}>
-              <div className={styles.imageContainer}>
-                <img src={Table} className={styles.leagueTable}></img>
-              </div>
-              <h3 className="textwhite">League Tables</h3>       
-              <p className="textwhite">Your hall is number #1 in the KnoWaste league.</p>
-              <Link to="gamification">
-              <button className='button-style-1'>View League</button>
-              </Link>
-          </div>
-    </div>
 
-    </div>
-        
+        </div>
+        <div className={styles.timer}>
+          <Timer />
+        </div>
+        <div className={`${styles.aboutUs} box-style-1`}>
+          <div className={styles.imageContainer}>
+            <img src={Logo} className={styles.leagueTable}></img>
+          </div>
+          <h3 className="textwhite">Your Impact</h3>
+          <p className="textwhite">Well done! 10% waste less than last week.</p>
+          <Link to="environment">
+          <button className='button-style-1'>Explore</button>
+          </Link>
+        </div>
+
+
+        <div className={`${styles.gamification} box-style-1`}>
+          <div className={styles.imageContainer}>
+            <img src={Table} className={styles.leagueTable}></img>
+          </div>
+          <h3 className="textwhite">League Tables</h3>
+          <p className="textwhite">Your hall is number #1 in the KnoWaste league.</p>
+          <Link to="gamification">
+          <button className='button-style-1'>View League</button>
+          </Link>
+        </div>
       </div>
     </div>
-  );
+
+  </div>
+);
 };
 
 export default Home;
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -15,8 +15,7 @@ const NavBar = () => {
     }
 
     return (
-        <div className="nav-box-desk">
-            <section className={`${styles.navBarContainer} box-style-4`}>
+        <nav className={`${styles['nav-bar']} nav-box-desk box-style-4`}>
                 <ul className={styles.mainNav}>
                     <li>
                         <Link to="/"> 
@@ -44,8 +43,7 @@ const NavBar = () => {
                         </Link>
                     </li>
                 </ul>
-            </section>
-        </div>
+        </nav>
     )
 }
 
