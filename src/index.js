b03f11f7a648f760a5ce4ce048662e9a37631ac5
Oliver Coldwell <olivercoldwell@gmail.com>
2021-03-24 16:29:25 +0000

    "changed index.js imports"

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App.jsx';
-import { auth, firestore } from "./firebase.js";
 import reportWebVitals from './reportWebVitals';
 
 
