74bdea1313951f64fe95f813755b3a253e89ebba
Oliver Coldwell <olivercoldwell@gmail.com>
2021-03-29 18:05:48 +0100

    changes to timebar

--- src/components/Timer/Timer.jsx
+++ src/components/Timer/Timer.jsx
@@ -2,48 +2,62 @@ import React, { useState, useEffect } from 'react'
 import styles from './Timer.module.scss'
 import { DateTime } from 'luxon';
 
-// TIMER FOR TESTING 
-   export const getTimeDifference = (time) => {
-  // 1. Get current date for comparison with upcoming friday
-  // const now = DateTime.now();
-  // 2. Get the upcoming friday
-  const upcomingFriday = 
-   (time.weekday <= 5) ? DateTime.local().startOf('week').plus({days: 5}): 
-                         DateTime.local().startOf('week').plus({days: 12});
-  // 3. Do the comparison to get the remaining time
-  const diff = upcomingFriday.diff(time, ["days", "hours", "minutes", "seconds", "milliseconds"])
+//USE THESE VARIABLES TO DEFINE DEADLINE
+// Luxon is not zero indexed, so Monday is equal to 1, Friday is equal to 5, so to get to Friday we add 4 days.
+let days = 4;
+let hours = 18;
+let now = DateTime.local()
+
+//Timer exported for testing
+export const setDeadline = (days, hours) => {
+  let deadlineThisWeek = DateTime.local().startOf('week').plus({days: days, hour: hours }) //Friday at 6pm
+  const deadline=  (now.weekday == deadlineThisWeek.weekday && now.hour > deadlineThisWeek.hour) ? deadlineThisWeek.plus({days: days + 7}) : deadlineThisWeek; 
+  return deadline;
+}
+//Time is the parameter we need to pass to mock for testing.
+  export const getTimeDifference = (time, days, hours) => {
+  let upcomingDeadline = setDeadline(days, hours)
+  const diff = upcomingDeadline.diff(time, ["days", "hours", "minutes", "seconds", "milliseconds"])
   return diff.values
 }  
 
-// the actual component
+export const lessThanSixHours = (days, hours) => (days == 0 && hours < 6) ? true : false;
+//The Timer component
 
 const Timer = () => {
 
   const [timeLeftStr, setTimeLeft] = useState("");
   const [isTimerRunout, setIsTimerRunout] = useState(false);
+  const [isRed, setRed] = useState(false);
 
   const getTimeDifference = () => {
-    // 1. Get current date for comparison with upcoming friday
-    const now = DateTime.now();
-    // 2. Get the upcoming friday
-    const upcomingFriday = 
-     (now.weekday <= 5) ? DateTime.local().startOf('week').plus({days: 5}): 
-                           DateTime.local().startOf('week').plus({days: 12});
-    // 3. Do the comparison to get the remaining time
-    const diff = upcomingFriday.diff(now, ["days", "hours", "minutes", "seconds", "milliseconds"])
+    let upcomingDeadline = setDeadline(days, hours)
+    const diff = upcomingDeadline.diff(now,["days", "hours", "minutes", "seconds", "milliseconds"])
     return diff.values
   }
-
+ const timerStyling = (d,h,m,s) => {
+   let hours = (h>1) ?`${h} hours`: `${h} hour`;
+   let days = (d>1) ?`${d} days`: `${d} day`;
+   let minutes = (m>1)?`${m} minutes`: `${m} minute`; 
+   return d > 0 ? `${days}` : 
+            `${hours}
+             ${minutes}
+             ${s}s`
+   }
+  
+ 
   useEffect(() => {
     let myInterval = setInterval(()=>{
       const diff = getTimeDifference();
       let{days, hours, minutes, seconds} = diff;
+      setRed(lessThanSixHours(days, hours));
+      timerStyling()
       if (minutes === 0 && seconds === 0) {
         setIsTimerRunout(true);
       } else {
         setIsTimerRunout(false);
       }
-      setTimeLeft(` ${days}day ${hours}hour ${minutes}min ${seconds}sec`)
+      setTimeLeft(timerStyling(days, hours, minutes, seconds))
     }, 1000)
     return ()=> {
       clearInterval(myInterval);
@@ -52,10 +66,11 @@ const Timer = () => {
   
   return (
     
-      <div className={`${styles.timer} textwhite box-style-1`}>
+      <div className={`${styles.timer} textwhite box-style-1 `}>
         { isTimerRunout? null
-              : <h2>{timeLeftStr} <span className="textwhite">until orders close</span></h2>
+              : <h2 className={isRed ? "textred" : "textwhite"}>{timeLeftStr} until orders close on Friday at 6pm</h2>
           }
+          
       </div>
     
   )
--- src/components/Timer/Timer.spec.js
+++ src/components/Timer/Timer.spec.js
@@ -1,41 +1,46 @@
 import jest from "jest";
 import  { DateTime } from "luxon";
-import { getTimeDifference } from './Timer';
-
-it ("Should return 8 hour difference for Friday @4pm", () => {
+import { getTimeDifference, lessThanSixHours, setDeadline } from './Timer';
+//Global variables to set up tests.
+let deadlinedays = 4;
+let deadlinehours = 18;
+it ("Should return 2 hour difference for Friday @4pm", () => {
     // Arrange / Setup
-    const fridayObject = DateTime.local().startOf('week').plus({days: 4, hours: 16})
-
+    let time = DateTime.local().startOf('week').plus({days:4, hours: 16})
     // Act / Run the code
-    const result = getTimeDifference(fridayObject);
+    const result = getTimeDifference(time, deadlinedays, deadlinehours);
 
     // Assert
-    expect (result.hours).toBe(8);
+    expect (result.hours).toBe(2);
 
 })
 
 it ("Should return 6h difference for Friday @6pm", () => {
     // Arrange / Setup
-    let fridayObject = DateTime.local().startOf('week').plus({days: 4, hours: 18})
-
+    let time = DateTime.local().startOf('week').plus({days: 4, hours: 18})
     // Act / Run the code
-    const result = getTimeDifference(fridayObject);
+    const result = getTimeDifference(time, deadlinedays, deadlinehours);
 
     // Assert
-    expect (result.hours).toBe(6);
+    expect (result.hours).toBe(0);
 
 
 })
 
 it ("Should return 6d 2h difference for Saturday @10pm", () => {
     // Arrange / Setup
-    let fridayObject = DateTime.local().startOf('week').plus({days: 5, hours: 22})
+    let time = DateTime.local().startOf('week').plus({days:4, hours: 22})
 
     // Act / Run the code
-    const result = getTimeDifference(fridayObject);
+    const result = getTimeDifference(time, deadlinedays, deadlinehours);
 
     // Assert
-    expect (result).toStrictEqual({days:6,hours:2,minutes:0,seconds:0,milliseconds:0});
+    expect (result).toStrictEqual({days:6,hours:8,minutes:0,seconds:0,milliseconds:0});
+})
 
-
+it ("Should be red text when the timer reaches 0 days and 5 hours", () => {
+    let fridayObject = DateTime.local().startOf('week').plus({days: 4, hours: 22});
+    const result = getTimeDifference(fridayObject);
+    const testSixHoursOrLess = lessThanSixHours(result.days, result.hours)
+    expect (testSixHoursOrLess).toBe(true)
 })
