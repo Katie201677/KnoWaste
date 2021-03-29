53a784ae7a26e7d455e8f65931b6b86729c50ad8
Alex Jordan <jordan.alex.g@gmail.com>
2021-03-29 16:02:41 +0100

    added getIDFromYYYYMMDD

--- src/services/weekid.service.js
+++ src/services/weekid.service.js
@@ -30,3 +30,20 @@ export const getNextWeekID = () => {
     
     return nextWeekId
 }
+
+export const getIDFromYYYYMMDD = (ISOdate) => {
+    //Date must be supplied as YYYYMMDD for this function to return ID
+
+    const date = DateTime.fromISO(ISOdate)
+    const day_of_week = date.weekday - 1
+
+    const lastMonday = date.minus({ days: day_of_week });
+    
+    const day = lastMonday.day.toString().padStart(2, '0')
+    const month = lastMonday.month.toString().padStart(2, '0')
+    const year = lastMonday.year.toString().slice(2)
+
+    const currentId = [year, month, day].join('')
+    
+    return currentId
+}
