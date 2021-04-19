b384dd9f6798adf197b384d5a858d859cc306b88
Ashlee <ashleeryan@outlook.com>
2021-04-19 11:10:08 +0100

    Updated firebase config with forked project details

--- src/firebase.js
+++ src/firebase.js
@@ -5,15 +5,13 @@ import "firebase/storage";
 
 
 export const firebaseConfig = {
-  apiKey: "AIzaSyCJypedQSVDr-lIILrJWSltWjcUQaHv6oc",
-  authDomain: "knowaste-3c92c.firebaseapp.com",
-  projectId: "knowaste-3c92c",
-  storageBucket: "knowaste-3c92c.appspot.com",
-  messagingSenderId: "1031894047710",
-  appId: "1:1031894047710:web:1fa6e25d8b273608e89b4f",
-  measurementId: "G-52P45TXG7Y"
+  apiKey: "AIzaSyCTgaevqF2xu2P1PY5ILNfACNGeftykdXg",
+  authDomain: "knowaste-ff8b9.firebaseapp.com",
+  projectId: "knowaste-ff8b9",
+  storageBucket: "knowaste-ff8b9.appspot.com",
+  messagingSenderId: "489884345590",
+  appId: "1:489884345590:web:6e1630a48d291a1bbaf8d7"
 };
-
 firebase.initializeApp(firebaseConfig);
 
 export const auth = firebase.auth();
