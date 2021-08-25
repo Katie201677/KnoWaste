d46b9420d4c307b20dc9373f8c5dc83b22908a8a
Katie Walker <katiewalker0517@gmail.com>
2021-08-25 14:40:03 +0100

    swap request component rendered and state updating

--- src/components/SwapForum/SwapForum.jsx
+++ src/components/SwapForum/SwapForum.jsx
@@ -1,19 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import NavBar from '../NavBar';
 import styles from "./SwapForum.module.scss";
 
+
+import SwapRequest from "./SwapRequest";
+
 const SwapForum = () => {
+  
+  const [ posts, setPosts ] = useState("");
+  
+  // const [ newPost, setNewPost ] = useState("");
+  
+  const updatePosts = (post) => {
+    console.log("event fired");
+    setPosts(
+      [post,
+      ...posts
+      ]
+    )
+  }
+
+  // const getPostsJsx = () => {
+  //   return (posts.map(post => <SwapRequest post={post} />));
+  // }
+
   return (
     <div>
       <div className={styles.content}>
         <NavBar />
-        <div className={styles.mainSection}>
 
-          <div>
-            <p>swaps here</p>
-          </div>
-
-        </div>
+        <section className={styles.mainSection}> 
+          <SwapRequest placeholder="Add your swap request..." updatePosts={updatePosts} />
+        {posts}
+        </section>
       </div>
     </div>
   )
---
+++ src/components/SwapForum/SwapRequest/SwapRequest.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import styles from "./SwapRequest.module.scss";
+import ProfilePic from "../../../assets/kitchen_1.jpg";
+
+const SwapRequest = (props) => {
+
+  const { updatePosts, placeholder } = props;
+  const [ value, setValue ] = useState("");
+
+
+  const handleSubmit = (event) => {
+    updatePosts(value);
+    event.preventDefault();
+    setValue("");
+  }
+  
+  return (
+    <div className={styles.addSwap}>
+      <img src={ProfilePic} className={styles.profile__pic}></img>
+      <form onSubmit={handleSubmit} className={styles.swapCard}>
+        <input 
+          className={styles.input}
+          type="text"
+          placeholder={placeholder}
+          onChange={e => setValue(e.target.value)}
+          value={value}
+        >
+        </input>
+        <button>Post</button>
+      </form>
+    </div>
+  )
+}
+
+export default SwapRequest
